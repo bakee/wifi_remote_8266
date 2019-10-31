533397a276765a95db90d8283888e18ec647cc5f
Md. Abdullah-Al-Bakee <mail.bakee@gmail.com>
2019-10-31 10:06:23 +0600

    Send initial broadcast

--- udp.ino
+++ udp.ino
@@ -1,28 +1,28 @@
 #include <WiFiUdp.h>
 
-#define BROADCAST_INTERVAL (20000)
+#define BROADCAST_INTERVAL (20 * 1000)
 
 WiFiUDP Udp;
 IPAddress udpIp;
 const unsigned int udpPort = 64101;
-unsigned long timeStamp = 0;
+unsigned long lastBroadcastTime = 0;
 
-void SendUdpPacket() {
-    String message = "Room: " + RoomName + ", IP: " + WiFi.localIP().toString();    
+void broadcast() {
+    String message = "Room: " + RoomName + ", MAC: " + WiFi.macAddress() + ", IP: " + WiFi.localIP().toString();    
     Udp.beginPacket(udpIp, udpPort);
     Udp.write(message.c_str());
     Udp.endPacket();
+    lastBroadcastTime = millis();
 }
 
 void udp_setup() {
-  udpIp.fromString("192.168.8.255");
-  timeStamp = millis();
-  Udp.begin(udpPort);
+  udpIp.fromString("192.168.8.255");  
+  Udp.begin(udpPort);  
+  broadcast();  
 }
 
 void udp_run() {
-    if(millis() - timeStamp > BROADCAST_INTERVAL) {
-    timeStamp = millis();
-    SendUdpPacket();
+  if((millis() - lastBroadcastTime) > BROADCAST_INTERVAL) {    
+    broadcast();
   }
 }
