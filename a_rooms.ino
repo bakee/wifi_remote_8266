7fe4f09345922859c8a47f4a316f5a22be53f6f2
Md. Abdullah-Al-Bakee <mail.bakee@gmail.com>
2019-10-31 09:44:36 +0600

    Move room names in a separate file and prefix the file with "a_" so that it is loaded before the other files and compiler finds the definition

---
+++ a_rooms.ino
@@ -0,0 +1,22 @@
+//const String RoomName = "South West";
+//const String RoomName = "South West Varanda";
+//
+//const String RoomName = "South East";
+//const String RoomName = "South East Varanda";
+//
+//const String RoomName = "East";
+//
+//const String RoomName = "Kitchen";
+//
+//const String RoomName = "Dining";
+//
+//const String RoomName = "North";
+//
+//const String RoomName = "Drawing North";
+//const String RoomName = "Drawing South";
+//
+//const String RoomName = "North West";
+//const String RoomName = "North West Varanda";
+//
+//const String RoomName = "Guest";
+//const String RoomName = "Guest Varanda";
--- http.ino
+++ http.ino
@@ -2,29 +2,6 @@
 
 ESP8266WebServer server(8081);
 
-//const String RoomName = "South West";
-//const String RoomName = "South West Varanda";
-//
-//const String RoomName = "South East";
-//const String RoomName = "South East Varanda";
-//
-//const String RoomName = "East";
-//
-//const String RoomName = "Kitchen";
-//
-//const String RoomName = "Dining";
-//
-//const String RoomName = "North";
-//
-//const String RoomName = "Drawing North";
-//const String RoomName = "Drawing South";
-//
-//const String RoomName = "North West";
-//const String RoomName = "North West Varanda";
-//
-//const String RoomName = "Guest";
-//const String RoomName = "Guest Varanda";
-
 String getResponse(String roomName) {
   String response = "<html><head>";
   response += "<meta http-equiv='refresh' content='15'>";
